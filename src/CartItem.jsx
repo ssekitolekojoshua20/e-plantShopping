b594f5da192610b07fd177445d7565816e56387b
ssekitolekojoshua20 <ssekitolekojoshua20@gmail.com>
2026-07-15 10:33:39 -0400

    cartitem updated

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,45 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+            let totalAmount = 0;
+
+        cart.forEach((item) => {
+            const itemCost = parseItemCostToInteger(item.cost);
+            totalAmount += itemCost * item.quantity;
+        });
+
+        return totalAmount;
  
   };
 
   const handleContinueShopping = (e) => {
-   
+        onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
+      if (item.quantity > 1) {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+  }
+  else{
+    dispatch(removeItem(item));
+}
    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+        const itemCost = parseItemCostToInteger(item.cost);
+        return item.quantity * itemCost;
   };
 
   return (
